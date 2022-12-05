1fa6e41893ef18199882d0ea164400d2f46ec80a
Donghyun Kim <dk58319@ajou.ac.kr>
2022-12-05 14:42:06 +0900

    FIX: multiple faces

--- src/SVM_test.py
+++ src/SVM_test.py
@@ -11,7 +11,7 @@ def is_hidden(path):
 
 def load_model():
     import pickle
-    with open('../model.pkl', 'rb') as f:
+    with open('./model.pkl', 'rb') as f:
         clf = pickle.load(f)
     return clf
 
@@ -21,9 +21,9 @@ def predict_multiple():
     from PIL import Image
     import numpy as np
     test_list = []
-    for i in os.listdir('../test'):
+    for i in os.listdir('./test'):
         if not is_hidden(i):
-            test_list.append('../test/'+i)
+            test_list.append('./test/'+i)
     for i in test_list:
         predict(i)
 
@@ -33,14 +33,18 @@ def predict(path):
     clf = load_model()
     img = PIL.Image.open(path)
     img_cropped = mtcnn(img, save_path=None)
-    print(len(img_cropped))
-    img_embedding = resnet(img_cropped[1].unsqueeze(0))
-    img_embedding = img_embedding.detach().numpy()
-    print(path)
-    print(clf.predict(img_embedding))
-    y_pred = np.amax(clf.predict_proba(img_embedding))
+    y_pred = []
+    print(str(len(img_cropped))+" faces found")
+    for i in range(len(img_cropped)):
+        img_embedding = resnet(img_cropped[i].unsqueeze(0))
+        img_embedding = img_embedding.detach().numpy()
+        if (np.amax(clf.predict_proba(img_embedding)) > 0.3).astype(bool):
+            y_pred.append(clf.predict(img_embedding))
+        else:
+            y_pred.append( 'unknown')
     print(y_pred)
 
+
 #predict_multiple()
-predict('../test_embed/article.jpg')
+predict('./test_embed/article.jpg')
 print('Done')
