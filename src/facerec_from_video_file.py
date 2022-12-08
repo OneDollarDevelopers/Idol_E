ad04c61df132cce7069092e4812e7553afa368ec
MinGeo <markhappy@naver.com>
2022-12-08 23:01:20 +0900

    auto video making complete

--- src/facerec_from_video_file.py
+++ src/facerec_from_video_file.py
@@ -4,8 +4,8 @@ from moviepy.editor import *
 
 import predict_module as M
 
-input_movie = cv2.VideoCapture("../test_video/example5.mp4")
-parent_clip = VideoFileClip('../test_video/example5.mp4', audio=True)
+input_movie = cv2.VideoCapture("../test_video/example6.mp4")
+parent_clip = VideoFileClip('../test_video/example6.mp4', audio=True)
 length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
 
 print("please input name:")
@@ -21,6 +21,7 @@ frame_number = 0
 start_time = 0
 finish_time = 0
 second = round(input_movie.get(cv2.CAP_PROP_FPS))
+print(second)
 flag= False
 while True:
     # Grab a single frame of video
@@ -33,32 +34,28 @@ while True:
     rgb_frame = frame[:, :, ::-1]
     face_names, face_encodings = M.predict(frame)
     if face_names == None:
-        #print('no face')
         continue
     if favorite_name in face_names:
-        flag=True
-        print("Nice!")
-        frame_number += int(input_movie.get(1))%second
+        flag = True
+        if int(input_movie.get(1))%second != 0:
+            temp_time = int(input_movie.get(1))//second
+            print(temp_time, int(input_movie.get(1)), frame_number)
+            temp_time += 1
+            frame_number = second * temp_time
+            print(frame_number)
         input_movie.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-        print(frame_number, int(input_movie.get(1)))
-
     if int(input_movie.get(1))%second == 0:
-        #print(int(input_movie.get(1)), frame_number)
-        print('Hello?')
         start_time += 1
         finish_time = start_time + 1
         if flag:
             videoclip.append(
                 parent_clip.subclip(start_time, finish_time)
             )
-            print('Nice!')
-            flag = False 
-        else:
-            continue
+            flag = False
 
 # All done!
 final_clip = concatenate_videoclips(videoclip)
-final_clip.write_videofile("../test_video/moviepy8.mp4",  codec='libx264', 
+final_clip.write_videofile("../test_video/moviepy9.mp4",  codec='libx264', 
                      audio_codec='aac', 
                      temp_audiofile='temp-audio.m4a', 
                      remove_temp=True)
